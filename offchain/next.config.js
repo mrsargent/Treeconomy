056f425a725f7dfce50828d980808575261ab8cb
Ryan Sargent <sargent11235@gmail.com>
2024-11-10 09:46:28 -0600

    working on rewards validator

---
+++ offchain/next.config.js
@@ -0,0 +1,49 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+ 
+//   output: 'standalone',
+//   reactStrictMode: true,
+//   webpack: (config) => {
+//     config.experiments = { 
+//       asyncWebAssembly: true,
+//       topLevelAwait: true,
+//       layers: true
+//     };
+//     // config.module.rules.push({
+//     //   test: /\.wasm$/,
+//     //   type: 'webassembly/async',
+//     // });
+//     return config
+//   },
+//     env:{
+//     BLOCKFROST_KEY: process.env.BLOCKFROST_KEY,
+//     API_URL: process.env.API_URL,
+//     NETWORK: process.env.NETWORK
+//   }
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+  output: 'standalone',
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.experiments = { 
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true
+    }
+    return config
+  },
+    env:{
+    BLOCKFROST_KEY: process.env.BLOCKFROST_KEY,
+    API_URL: process.env.API_URL,
+    NETWORK: process.env.NETWORK
+  }
+}
+
+module.exports = nextConfig
