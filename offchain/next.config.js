036ec82dc2102083fa013d058c29f2548dc6d9c6
Ryan Sargent <sargent11235@gmail.com>
2024-11-10 20:05:59 -0600

    changed some config shit

--- offchain/next.config.js
+++ offchain/next.config.js
@@ -26,9 +26,31 @@
 // export default nextConfig;
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+  
+//   output: 'standalone',
+//   reactStrictMode: true,
+//   webpack: (config) => {
+//     config.experiments = { 
+//       asyncWebAssembly: true,
+//       topLevelAwait: true,
+//       layers: true
+//     }
+//     return config
+//   },
+//     env:{
+//     BLOCKFROST_KEY: process.env.BLOCKFROST_KEY,
+//     API_URL: process.env.API_URL,
+//     NETWORK: process.env.NETWORK
+//   }
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
   output: 'standalone',
   reactStrictMode: true,
   webpack: (config) => {
@@ -36,14 +58,18 @@ const nextConfig = {
       asyncWebAssembly: true,
       topLevelAwait: true,
       layers: true
-    }
-    return config
+    };
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+    return config;
   },
-    env:{
+  env: {
     BLOCKFROST_KEY: process.env.BLOCKFROST_KEY,
     API_URL: process.env.API_URL,
     NETWORK: process.env.NETWORK
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ offchain/postcss.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+module.exports = config;
